85f0861f3c5a3f476e925b97409445670ce17bd6
Luke Channings <Luke.Channings@akqa.com>
2016-05-15 22:35:39 +0200

    Add some modules.

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,14 @@
 # FISH plugins
 fundle plugin 'edc/bass'
 fundle plugin 'tuvistavie/oh-my-fish-core'
-fundle init
+fundle plugin 'tuvistavie/fish-completion-helpers'
+fundle plugin 'oh-my-fish/plugin-foreign-env'
+fundle plugin 'tuvistavie/fish-nvm'
+fundle plugin 'TheFuzzball/flash'
+fundle init --profile
+
+# Node Version manager
+nvm use v5 > /dev/null ^ /dev/null &
+
+# configure FZF to ignore files listed in various ignorefiles with ag.
+set -gx FZF_DEFAULT_COMMAND='ag -g ""'
