02595950253f7bfbd7a24ae67aebcdb73f4bab04
Luke Channings <Luke.Channings@akqa.com>
2016-06-13 15:58:22 +0200

    Fix issue with copy not working in 10.11.

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,7 @@ fundle init
 nvm use v5 > /dev/null ^ /dev/null &
 
 # Configure FZF to ignore files listed in various ignorefiles with ag.
-set -gx FZF_DEFAULT_COMMAND='ag -g ""'
+set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
 
 # Configure Fish welcome message
 set fish_greeting
