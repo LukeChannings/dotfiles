aa58d7d0144a03a789d306778688ad94c4a1f8a9
Luke Channings <L.Channings@amaze.com>
2016-11-28 14:10:44 +0000

    gitco: exclude current branch, remove remotes/ prefix, dedupe.

--- fish/functions/gitco.fish
+++ fish/functions/gitco.fish
@@ -1,3 +1,3 @@
 function gitco -d "Fuzzy-find and checkout a branch"
-  git branch --all | grep -v HEAD | string trim | fzf | xargs git checkout
+  git branch --all | grep -v '*' | string replace -r 'remotes/.*?/' '' | sort -u | fzf | xargs git checkout
 end
