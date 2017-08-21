e0f72d27bccdfa40a4f0d46439101af4539f1687
Luke Channings <luke@channings.me>
2017-08-21 22:24:06 +0100

    Add a sketch 43.1 Homebrew formula

---
+++ homebrew/cask/sketch-43-1.rb
@@ -0,0 +1,23 @@
+cask 'sketch-43-1' do
+  version '43.1-39012'
+  sha256 '7e80b4c708665e285048fa2668a11556fd0fbf7d165709f9af46ca7011bec52f'
+
+  url "https://download.sketchapp.com/sketch-#{version}.zip"
+  appcast 'https://download.sketchapp.com/sketch-versions.xml',
+          checkpoint: 'ff42f4eea2945b2a299786cb917e6463fec38698b0bf45a64a83881d22be4ae3'
+  name 'Sketch'
+  homepage 'https://www.sketchapp.com/'
+
+  app 'Sketch.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl',
+                '~/Library/Application Support/com.bohemiancoding.sketch3',
+                '~/Library/Caches/com.bohemiancoding.sketch3',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
+                '~/Library/Logs/com.bohemiancoding.sketch3',
+                '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
+                '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
+                '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
+              ]
+end
