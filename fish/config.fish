8e7c40c5c82e15fecb959a707c231db367f19c0b
Luke Channings <luke@channings.me>
2017-09-27 18:10:08 -0500

    Update fundle dep

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'oh-my-fish/plugin-foreign-env'
 fundle plugin 'tuvistavie/fish-nvm'
-fundle plugin 'oh-my-fish/theme-l'
+fundle plugin 'LukeChannings/theme-l'
 fundle plugin 'brgmnn/fish-docker-compose'
 fundle init
 
@@ -16,4 +16,3 @@ set -gx FZF_DEFAULT_COMMAND '/usr/local/bin/ag -g ""'
 set -gx fish_greeting
 set -gx LESSOPEN "| "(which highlight)" --out-format xterm256 -s Zenburn --quiet --force %s"
 set -gx LESS " -R -X -F "
-
