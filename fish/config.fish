828c4d52be859924eea904c530b8cce7cd32a2bf
Luke Channings <luke@channings.me>
2017-10-22 00:55:42 +0100

    Move FZF backend to ripgrep from ag. Add gist cli

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,7 @@ fundle init
 set -gx EDITOR vim
 set -gx VISUAL vim
 set -gx GIT_EDITOR vim
-set -gx FZF_DEFAULT_COMMAND '/usr/local/bin/ag -g ""'
+set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 set -gx fish_greeting
 set -gx LESSOPEN "| "(which highlight)" --out-format xterm256 -s Zenburn --quiet --force %s"
 set -gx LESS " -R -X -F "
