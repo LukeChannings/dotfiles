8b4f5879f5415fadbf3059725d37458211de67dd
Luke Channings <luke@channings.me>
2019-03-18 09:25:30 -0400

    Switch to my theme fork.

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'oh-my-fish/plugin-foreign-env'
 fundle plugin 'tuvistavie/fish-nvm'
-fundle plugin 'oh-my-fish/theme-l'
+fundle plugin 'LukeChannings/theme-l'
 fundle plugin 'brgmnn/fish-docker-compose'
 fundle init
 
@@ -16,3 +16,4 @@ set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!{.git,node_mo
 set -gx fish_greeting
 set -gx LESSOPEN "| "(which highlight)" --out-format xterm256 -s Zenburn --quiet --force %s"
 set -gx LESS " -R -X -F "
+
