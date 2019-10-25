7a3627234dd805b20fcb21cf715b87e5a0e67e58
Luke Channings <luke@channings.me>
2019-10-25 11:14:24 +0100

    Add git checkout history via '--'

--- fish/functions/gitco.fish
+++ fish/functions/gitco.fish
@@ -1,5 +1,7 @@
 function gitco -d "Fuzzy-find and checkout a branch"
-  if test $argv[1]
+  if test $argv[1] = '--'
+    git reflog | egrep -io "moving from ([^[:space:]]+)" | awk '{ print $3 }' | awk ' !x[$0]++' | egrep -v '^[a-f0-9]{40}$' | fzf | xargs git checkout
+  else if test $argv[1]
     git checkout $argv
   else
     git branch --all | grep -v '*' | string replace -r 'remotes/.*?/' '' | sort -u | fzf | xargs git checkout
