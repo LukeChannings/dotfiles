061dc13db29bedfbc3ae05ee76057f92334df966
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2024-09-17 15:52:38 +0100

    Add personal cachix cache to substitutors

--- config/nix/config.nix
+++ config/nix/config.nix
@@ -11,10 +11,12 @@
   trusted-users = builtins.attrNames users;
   trusted-public-keys = [
     "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    "luke-channings.cachix.org-1:ETsZ3R5ue9QOwO4spg8aGJMwMU6k5tQIaHWnTakGHjo="
   ];
 
   substituters = [
     "https://devenv.cachix.org"
+    "https://luke-channings.cachix.org"
   ];
 
   max-substitution-jobs = 128;
