6b63868d13882ca9703fb42339e1cd8cff683c2f
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2024-10-19 13:48:28 +0100

    Remove linux-builder

--- config/nix/darwin.nix
+++ config/nix/darwin.nix
@@ -17,26 +17,5 @@
     };
 
     settings = (import ./config.nix { users = config.users.users; });
-
-    gc.automatic = true;
-
-    linux-builder = {
-      enable = true;
-
-      # nixpkgs-unstable often does not have a cached linux-builder,
-      # and we can't compile this ourselves without... a functioning linux-builder.
-      # I'm pinning to a stable nixpkgs, where the package should always be cached.
-      package = inputs.nixpkgs-darwin.legacyPackages.${pkgs.system}.darwin.linux-builder;
-
-      config = {
-        virtualisation = {
-          darwin-builder = {
-            diskSize = 80 * 1024;
-            memorySize = 24 * 1024;
-          };
-          cores = 6;
-        };
-      };
-    };
   };
 }
