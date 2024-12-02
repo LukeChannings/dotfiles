cafcfb3ea30e540f81c792d40b47a2f085b31e6d
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2024-12-02 12:17:28 +0000

    Update Apparency sha

--- config/brew/darwin.nix
+++ config/brew/darwin.nix
@@ -18,7 +18,7 @@
         in
         super.brewCasks
         // (builtins.mapAttrs overrideHash {
-          apparency = "sha256-olx6q3Vms526EJRgNhv/PlYLfBa2VPEDedS9S3uCAKE=";
+          apparency = "sha256-nktNbyJOXDydQPwb43Uq3nQv65XcwrqZTKu5OCcLyfU=";
           suspicious-package = "sha256-//iL7BRdox+KA1CJnGttUQiUfskuBeMGrf1YUNt/m90=";
           bonjour-browser = "sha256-+crlyoU0zPu+oilrTyLIOO61H7U9bkyDWe8EpWJfnOQ=";
           audio-hijack = "sha256-hzZbGfM0h7nrEqatRFMtTh3NQ0yFgGEyUVawvk3LpaQ=";
