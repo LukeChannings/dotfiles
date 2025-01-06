8ebc8a9b03d32017ea2904373735bcb623d31845
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2025-01-06 18:01:18 +0000

    Formatting

--- config/wezterm/home.nix
+++ config/wezterm/home.nix
@@ -8,12 +8,16 @@
   config.programs.wezterm = {
     enable = true;
 
-    extraConfig = builtins.replaceStrings [ "@multiplexingName@" ] [
-      (lib.toLower (
-        "${
-          (if osConfig.networking.hostName != null then osConfig.networking.hostName else "wezterm")
-        }_${config.home.username}"
-      ))
-    ] (builtins.readFile ./config.lua);
+    extraConfig =
+      builtins.replaceStrings
+        [ "@multiplexingName@" ]
+        [
+          (lib.toLower (
+            "${
+              (if osConfig.networking.hostName != null then osConfig.networking.hostName else "wezterm")
+            }_${config.home.username}"
+          ))
+        ]
+        (builtins.readFile ./config.lua);
   };
 }
