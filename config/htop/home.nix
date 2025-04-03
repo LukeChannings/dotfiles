c6c95ebe8da1eda5cf2063e3a578af5fd955df97
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2025-04-03 23:55:22 +0100

    Remove htop settings

--- config/htop/home.nix
+++ config/htop/home.nix
@@ -1,49 +1,6 @@
-{ config, ... }:
+{ ... }:
 {
   config.programs.htop = {
     enable = true;
-    settings =
-      {
-        color_scheme = 6;
-        cpu_count_from_one = 0;
-        delay = 15;
-        fields = with config.lib.htop.fields; [
-          PID
-          USER
-          PRIORITY
-          NICE
-          M_SIZE
-          M_RESIDENT
-          M_SHARE
-          STATE
-          PERCENT_CPU
-          PERCENT_MEM
-          TIME
-          COMM
-        ];
-        highlight_base_name = 1;
-        highlight_megabytes = 1;
-        highlight_threads = 1;
-        screen_tabs = 1;
-        show_cpu_temperature = 1;
-        tree_view = 1;
-      }
-      // (
-        with config.lib.htop;
-        leftMeters [
-          (bar "AllCPUs2")
-          (bar "Memory")
-          (bar "Swap")
-        ]
-      )
-      // (
-        with config.lib.htop;
-        rightMeters [
-          (text "Tasks")
-          (text "LoadAverage")
-          (text "Uptime")
-          (text "Systemd")
-        ]
-      );
   };
 }
