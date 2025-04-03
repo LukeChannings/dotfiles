6826e8c7b8b3f0bf59dafd0e3f924499d1054ff3
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2025-04-03 11:35:09 +0100

    Update Nu config

--- config/nushell/home.nix
+++ config/nushell/home.nix
@@ -1,11 +1,7 @@
 {
   pkgs,
-  lib,
   ...
 }:
-let
-  inherit (pkgs) stdenv darwin;
-in
 {
   config = {
     programs.nushell = {
@@ -16,29 +12,10 @@ in
       loginFile.text = (builtins.readFile ./login.nu);
     };
 
-    home.packages =
-      let
-        fixupForDarwin =
-          drv:
-          drv.overrideAttrs (oldAttrs: {
-            buildInputs =
-              oldAttrs.buildInputs
-              ++ lib.optionals stdenv.isDarwin [
-                darwin.apple_sdk.frameworks.IOKit
-                darwin.apple_sdk.frameworks.Security
-                darwin.apple_sdk.frameworks.CoreServices
-              ];
-          });
-      in
-      (map fixupForDarwin (
-        with pkgs.nushellPlugins;
-        [
-          formats
-          polars
-          query
-          #          net
-          #          units
-        ]
-      ));
+    home.packages = with pkgs.nushellPlugins; [
+      formats
+      polars
+      query
+    ];
   };
 }
