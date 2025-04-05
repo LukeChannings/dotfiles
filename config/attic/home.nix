d56c46bf66dd3d88ef0d5be840f17bbd3b23f4cf
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2025-04-05 20:45:47 +0100

    Add attic watch daemon

---
+++ config/attic/home.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  options.dotfiles.attic = {
+    watch-daemon = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkEnableOption "Enable the attic watch-store daemon";
+          cache = lib.mkOption {
+            type = lib.types.str;
+            description = "The cache to push to";
+          };
+        };
+      };
+    };
+  };
+
+  config = {
+    home.packages = [
+      pkgs.attic-client
+    ];
+
+    launchd.agents = lib.mkIf config.dotfiles.attic.watch-daemon.enable {
+      attic =
+        let
+          inherit (config.home) homeDirectory;
+        in
+        {
+          enable = true;
+          config.ProgramArguments = [
+            (lib.getExe pkgs.attic-client)
+            "watch-store"
+            config.dotfiles.attic.watch-daemon.cache
+          ];
+          config.RunAtLoad = true;
+          config.KeepAlive = true;
+          config.HardResourceLimits.NumberOfFiles = 6000;
+          config.LowPriorityIO = true;
+          config.ProcessType = "Background";
+          config.StandardErrorPath = "${homeDirectory}/Library/Logs/attic-watch-daemon.log";
+          config.StandardOutPath = "${homeDirectory}/Library/Logs/attic-watch-daemon.log";
+          config.EnvironmentVariables = {
+            HOME = homeDirectory;
+            XDG_CONFIG_HOME = "${homeDirectory}/.config";
+          };
+        };
+    };
+  };
+}
