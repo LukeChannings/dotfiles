04f4246b4a9c5c72bcfbcd829fcccb3f495cdbc4
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2025-04-07 11:07:15 +0100

    Add PCA root

---
+++ config/certificate-authority/nixos+darwin.nix
@@ -0,0 +1,19 @@
+{
+  security.pki.certificates = [
+    # Channings Principal Certificate Authority Root
+    ''
+      -----BEGIN CERTIFICATE-----
+      MIIByjCCAXGgAwIBAgIQQofn+rGxJXU/EI5Z1irlHzAKBggqhkjOPQQDAjBEMRww
+      GgYDVQQKExNDaGFubmluZ3MgUHJpbmNpcGFsMSQwIgYDVQQDExtDaGFubmluZ3Mg
+      UHJpbmNpcGFsIFJvb3QgQ0EwHhcNMjUwMTMxMTgyNjUwWhcNMzUwMTI5MTgyNjUw
+      WjBEMRwwGgYDVQQKExNDaGFubmluZ3MgUHJpbmNpcGFsMSQwIgYDVQQDExtDaGFu
+      bmluZ3MgUHJpbmNpcGFsIFJvb3QgQ0EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC
+      AARji9/y0detw3pZlmoythdnAUgikjJrU3u1Ty/dXPtyfnfgd9RpRhtdIC5P34JF
+      5BXtXhO1ZRK6QcYO8epKkUWco0UwQzAOBgNVHQ8BAf8EBAMCAQYwEgYDVR0TAQH/
+      BAgwBgEB/wIBATAdBgNVHQ4EFgQULNjZRx+vQr7P0uHOLnPBzQ+HmGwwCgYIKoZI
+      zj0EAwIDRwAwRAIgSCosJwohwbcJkU5t7nQtg/Svrd6gM5Te7kZQaVsOGPkCIHEx
+      7v+ZhKQj+ApmSIxkASPYIkonlG4XlZwrfKCCZE1I
+      -----END CERTIFICATE-----
+    ''
+  ];
+}
